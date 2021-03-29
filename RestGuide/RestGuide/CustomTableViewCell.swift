9753df90d9c324a0ca206ed31f40e0ce605f5976
Moe Hosseini <moeabdolhosseini@gmail.com>
2021-03-28 22:37:20 -0400

    UI design

---
+++ RestGuide/RestGuide/CustomTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.swift
+//  RestGuide
+//
+//  Created by Tech on 2021-03-28.
+//  Copyright © 2021 gbc. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var restName: UILabel!
+    
+    @IBOutlet weak var restDesc: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
